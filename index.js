750056bc9756c6334ebd89902c81a79b35b282ad
Alexandre Ladeira <alexandre.ladeira@gmail.com>
2024-02-20 11:16:51 +0000

    add api/users get and post endpoints

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.MONGODB_URI);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
 });
 
+const userSchema = new mongoose.Schema({
+  username: String,
+  exercises: [{ description: String, duration: String, date: Date }],
+});
+const User = mongoose.model("User", userSchema);
 
-
-
+app
+  .route("/api/users")
+  .get(async (req, res) => {
+    const result = await User.find({}, "username _id");
+    return res.json(
+      result.map((user) => ({ _id: user._id, username: user.username }))
+    );
+  })
+  .post(async (req, res) => {
+    const user = new User({ username: req.body.username });
+    await user.save();
+    return res.json({ _id: user._id, username: user.username });
+  });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
